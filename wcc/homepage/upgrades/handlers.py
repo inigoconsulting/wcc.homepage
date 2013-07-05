68be553704174db1f3343777e1186cd2a4703c29
Izhar Firdaus <izhar@inigo-tech.com>
2013-07-05 14:30:12 +0800

    added upgrade profile to install homepage2 and homepage3 types (re #355, re #354)

--- wcc/homepage/upgrades/handlers.py
+++ wcc/homepage/upgrades/handlers.py
@@ -2,3 +2,12 @@ from collective.grok import gs
 from Products.CMFCore.utils import getToolByName
 
 # -*- extra stuff goes here -*- 
+
+
+@gs.upgradestep(title=u'Upgrade wcc.homepage to 1001',
+                description=u'Upgrade wcc.homepage to 1001',
+                source='1', destination='1001',
+                sortkey=1, profile='wcc.homepage:default')
+def to1001(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-wcc.homepage.upgrades:to1001')
